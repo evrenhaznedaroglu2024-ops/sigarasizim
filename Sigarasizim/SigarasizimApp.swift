8bf55261fff57028da8541067edc01403c046459
Evren Haznedaroğlu <evrenhaznedaroglu@gmail.com>
2026-01-18 00:11:49 +0300

    Initial Commit

---
+++ Sigarasizim/SigarasizimApp.swift
@@ -0,0 +1,17 @@
+//
+//  SigarasizimApp.swift
+//  Sigarasizim
+//
+//  Created by Evren on 18.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct SigarasizimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
